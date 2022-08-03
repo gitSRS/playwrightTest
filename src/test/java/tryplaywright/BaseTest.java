352a0fa909d53c2dff9039875fd94806556156ed
user <streltsovrs@gmail.com>
2022-08-03 21:22:18 +0300

    init commit

---
+++ src/test/java/tryplaywright/BaseTest.java
@@ -0,0 +1,11 @@
+package tryplaywright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Playwright;
+
+public class BaseTest {
+    // Shared between all tests in this class.
+    static Playwright playwright;
+    static Browser browser;
+
+}
